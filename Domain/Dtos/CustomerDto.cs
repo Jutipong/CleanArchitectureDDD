dfdff99da750418be98e21a1c38289840ceacd49
Js.dev@macbook <techcom.ec20@gmail.com>
2023-12-04 23:42:29 +0700

    Update CustomerDto and CustomerEntities classes

--- Domain/Dtos/CustomerDto.cs
+++ Domain/Dtos/CustomerDto.cs
@@ -2,8 +2,8 @@
 public class CustomerDto
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Code { get; set; }
+    public string? Name { get; set; }
+    public string? Code { get; set; }
 }
 
 
--- Domain/Dtos/CustomerEntities.cs
+++ Domain/Dtos/CustomerEntities.cs
@@ -2,13 +2,13 @@
 public class CustomerEntities
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Code { get; set; }
+    public string? Name { get; set; }
+    public string? Code { get; set; }
 }
 
 public class CustomerEntities2
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Code { get; set; }
+    public string? Name { get; set; }
+    public string? Code { get; set; }
 }
