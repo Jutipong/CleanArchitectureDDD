243f16b504abbbab01897966176284e9c76a0bc5
T660046@tcrbank.com <T660046@tcrbank.com>
2024-06-25 16:05:42 +0700

    update api.controller

--- api.controller/Controllers/CustomerController.cs
+++ api.controller/Controllers/CustomerController.cs
@@ -1,14 +1,57 @@
-using Microsoft.AspNetCore.Mvc;
+using Application.Customer.Create;
+using Application.Customer.Dapper;
+using Application.Customer.Delete;
+using Application.Customer.GetById;
+using Application.Customer.Inquiry;
+using Application.Customer.Update;
 
 namespace Api.Controller.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class CustomerController : ControllerBase
+public class CustomerController(ISender sender) : ControllerBase
 {
-    [HttpPost(Name = "Inquiry")]
-    public ActionResult Inquiry()
+    private readonly ISender _sender = sender;
+
+    [HttpPost(Name = "Create")]
+    public async Task<ActionResult> CreateAsync(CustomerCreateCommand req, CancellationToken token)
     {
-        return Ok("dddddddddddddddd");
+        var result = await _sender.Send(req, token);
+        return Ok(result);
+    }
+
+    [HttpPost(Name = "Update")]
+    public async Task<ActionResult> UpdateAsync(CustomerUpdateCommand req, CancellationToken token)
+    {
+        var result = await _sender.Send(req, token);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}", Name = "Delete")]
+    public async Task<ActionResult> DeleteAsync(Guid id, CancellationToken token)
+    {
+        var result = await _sender.Send(new CustomerDeleteCommand(id), token);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}", Name = "GetById")]
+    public async Task<ActionResult> GetByIdAsync(Guid id, CancellationToken token)
+    {
+        var result = await _sender.Send(new CustomerGetByIdQuery(id), token);
+        return Ok(result);
+    }
+
+    [HttpPost(Name = "Inquiry")]
+    public async Task<ActionResult> InquiryAsync(CustomerInquiryQuery req, CancellationToken token)
+    {
+        var result = await _sender.Send(req, token);
+        return Ok(result);
+    }
+
+    [HttpPost(Name = "Demo_EfCore_Dapper")]
+    public async Task<ActionResult> DemoEfCoreDapperAsync(CustomerDapperHandlerQuery req, CancellationToken token)
+    {
+        var result = await _sender.Send(req, token);
+        return Ok(result);
     }
 }
