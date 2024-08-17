1fb5692cbf31dd15ae116cfe5a74c74e8c82ad23
Jutipong <techcom.ec20@gmail.com>
2024-08-17 23:54:38 +0700

    chore: Update appsettings.json and AppSettings.cs

--- Domain/Abstractions/AppSettings.cs
+++ Domain/Abstractions/AppSettings.cs
@@ -1,5 +1,3 @@
 namespace Domain.Abstractions;
 
-public record AppSettings(string SystemId, string ProjectName, string AppVersion, Endpoints Endpoints);
-
-public record Endpoints(string LinkAS400, string LinkUserManagement);
+public record AppSettings(string SystemId, string ProjectName, string AppVersion);
