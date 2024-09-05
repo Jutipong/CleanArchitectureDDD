dc3fd313fc03c8bcb68ae5633856baab58395742
T660046@tcrbank.com <T660046@tcrbank.com>
2024-09-05 16:44:35 +0700

    refactor:  SerializerOptions

--- Api.Controller/Extensions/SerializeOption.cs
+++ Api.Controller/Extensions/SerializeOption.cs
@@ -1,14 +1,14 @@
-using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
-
 namespace Api.Controller.Extensions;
 
 public static class SerializeOption
 {
     public static void AddJsonOptions(this IServiceCollection services)
     {
-        services.Configure<JsonOptions>(options =>
-        {
-            options.SerializerOptions.PropertyNamingPolicy = null;
-        });
+        services
+            .AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
     }
 }
--- Api.Minimal/Extensions/SerializeOption.cs
+++ Api.Minimal/Extensions/SerializeOption.cs
@@ -1,12 +1,10 @@
-using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
-
 namespace Api.Minimal.Extensions;
 
 public static class SerializeOption
 {
     public static void AddJsonOptions(this IServiceCollection services)
     {
-        services.Configure<JsonOptions>(options =>
+        services.ConfigureHttpJsonOptions(options =>
         {
             options.SerializerOptions.PropertyNamingPolicy = null;
         });
